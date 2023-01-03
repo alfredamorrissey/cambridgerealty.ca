a136968d7ac034db392734c1c0856437505d8efe
Alfreda Morrissey <amorris6@uottawa.ca>
2023-01-03 10:31:07 -0500

    Remove old version for php 5 with old google maps code in source

--- cambridgerealty/mobile/js/vendor/upload_photos.js
+++
@@ -1,4 +0,0 @@
-(function(){dust.register("uploadingImage",body_0);
-function body_0(chk,ctx)
-{
-	return chk.w("<li id=\"").f(ctx.get(["imageId"], false),ctx,"h").w("\" data-image-id=\"").f(ctx.get(["imageId"], false),ctx,"h").w("\" class=\"uploading\"><a href=\"#\" class=\"remove kj-icon-cancel\"></a><div class=\"container\" style=\"display: block;\"><div class=\"ribbon\"><div class=\"spinner animate-spin kj-icon-spin4\"></div><div class=\"image-name\">").f(ctx.get(["imageName"], false),ctx,"h").w("</div></div></div></li>");}body_0.__dustBody=!0;return body_0;})();
